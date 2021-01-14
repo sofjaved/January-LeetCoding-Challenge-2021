8cb60502f4995c3f221271fa2faf79a3687aed42
Sofia Javed <sofjaved@gmail.com>
2021-01-13 22:32:23 -0500

    jan 13 added

---
+++ Jan13/mySolution.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} people
+ * @param {number} limit
+ * @return {number}
+ */
+var numRescueBoats = function(people, limit) {
+  people.sort((a,b) => a-b);
+  let left = 0;
+  let right = people.length-1;
+  let count = 0
+  while(left <= right) {
+      if(people[left] + people[right] > limit) {
+          count++
+          right--;
+      } else {
+          count++;
+          left++;
+          right--;
+      }
+  }
+  return count;
+};
